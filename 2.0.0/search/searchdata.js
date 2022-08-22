307c716996ec174fe1caa57e81794e654a5fd776
jeanpierrefortune <jeanpierrefortune@users.noreply.github.com>
2022-08-22 17:10:45 +0000

    deploy: 1532561612e430827f2ddba7510053a5870b5ff9

---
+++ 2.0.0/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "cgk~",
+  1: "k",
+  2: "k",
+  3: "ck",
+  4: "g~"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions"
+};
+
--- list_versions.md
+++ list_versions.md
@@ -1,2 +1,3 @@
 | Version | Documents |
 |:---:|---|
+| 2.0.0 | [API documentation](2.0.0) |
